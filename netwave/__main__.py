f4ad55ab687420e5d9b991909c66fcc7a7d9dbf4
TheLogicMaster <justinmarentette11@gmail.com>
2020-05-19 06:31:24 -0400

    CLI Finally correct?

--- netwave/__main__.py
+++ netwave/__main__.py
@@ -267,7 +267,8 @@ def main():
                                 choices=['stop_movement', 'move_up', 'move_down', 'move_left', 'move_right',
                                          'move_up_left', 'move_up_right', 'move_down_left', 'move_down_right',
                                          'move_center', 'patrol_vertical', 'stop_patrol_vertical', 'patrol_horizontal',
-                                         'stop_patrol_horizontal', 'turn_io_on', 'turn_io_off', 'set_preset',
+                                         'stop_patrol_horizontal', 'pelco_patrol_horizontal',
+                                         'pelco_stop_patrol_horizontal', 'turn_io_on', 'turn_io_off', 'set_preset',
                                          'recall_preset', 'restart_camera', 'factory_reset_camera'])
     command_parser.add_argument('--preset', type=int, help='Preset id for saved position(1-15)', default=1)
 
